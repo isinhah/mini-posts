ba69a4807d96146b6de1846d77c9e73fe95b4ee6
Isabel Henrique <6isinha6@gmail.com>
2024-02-07 16:37:44 -0300

    usando padrão DTO para retornar os usuarios

--- src/main/java/com/dev/springmongodb/resources/UserResource.java
+++ src/main/java/com/dev/springmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.dev.springmongodb.resources;
 
 import com.dev.springmongodb.domain.User;
+import com.dev.springmongodb.dto.UserDTO;
 import com.dev.springmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO((x))).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
