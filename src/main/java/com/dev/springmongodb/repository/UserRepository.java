eefd627618e408ad1e43f27acda12bd503b03ea2
Isabel Henrique <6isinha6@gmail.com>
2024-02-07 15:44:57 -0300

    repository e service

---
+++ src/main/java/com/dev/springmongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dev.springmongodb.repository;
+
+import com.dev.springmongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+}
---
+++ src/main/java/com/dev/springmongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.dev.springmongodb.services;
+
+import com.dev.springmongodb.domain.User;
+import com.dev.springmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
